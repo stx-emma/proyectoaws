c05c1c3276b9eb3004c13aee01b286f065cfca89
Emmanuel González <ggemma931@gmail.com>
2025-04-01 11:09:16 -0600

    Update config.php

--- RegistroUsuario/config.php
+++ RegistroUsuario/config.php
@@ -1,6 +1,5 @@
 <?php
 
-//$conexion = mysqli_connect("bases.dis.uaemex.mx", "davaldesa", "davaldesa", "dbsibidi");
 $conexion = mysqli_connect("dbregmex:3306", "root", "ema", "dbregmex");      
 if (mysqli_connect_errno() ){
   echo "No se puede conectar a la base: " . mysqli_connect_error();
